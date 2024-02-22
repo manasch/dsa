4f1f1b2cc711400a92f9b0f57582485a91dc435f
Manas Chebrolu <75514487+manasch@users.noreply.github.com>
2024-02-22 18:06:51 +0530

    aoc day14 p2

--- aoc/aoc2023/day14/main.py
+++ aoc/aoc2023/day14/main.py
@@ -47,7 +47,49 @@ class Solution:
         # print("\n".join(r2))      
 
     def solveTwo(self):
-        pass
+        dish = [[ch for ch in row] for row in self.input]
+
+        def cycle():
+            nonlocal dish
+            for _ in range(4):
+                for j in range(len(dish[0])):
+                    count = 0
+                    for i in range(len(dish) - 1, -1 , -1):
+                        if dish[i][j] == 'O':
+                            count += 1
+                            dish[i][j] = '.'
+                        elif dish[i][j] == '#':
+                            for k in range(count):
+                                dish[i + k + 1][j] = 'O'
+                            count = 0
+                    if count > 0:
+                        for k in range(count):
+                            dish[i + k][j] = 'O'
+                dish = [list(x) for x in zip(*reversed(dish))]
+        
+        temp = tuple(tuple(row) for row in dish)
+        gridSet = {temp}
+        gridArray = [temp]
+        itr = 0
+
+        while True:
+            itr += 1
+            cycle()
+            temp = tuple(tuple(row) for row in dish)
+            if temp in gridSet:
+                break
+            gridSet.add(temp)
+            gridArray.append(temp)
+        
+        first = gridArray.index(temp)
+
+        finalGridIdx = (1000000000 - first) % (itr - first) + first
+        finalGrid = gridArray[finalGridIdx]
+
+        res = 0
+        for r, row in enumerate(finalGrid):
+            res += (len(finalGrid) - r) * row.count('O')
+        self.partTwo = res
 
     def solve(self):
         self.parse()
